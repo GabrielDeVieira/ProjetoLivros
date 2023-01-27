c6fb7ff964f71bdba4fda8c74f9f4ad28f3e4707
GabrielDeVieira <87400529+GabrielDeVieira@users.noreply.github.com>
2023-01-27 05:40:56 -0300

    Update views.py

--- livros/views.py
+++ livros/views.py
@@ -12,30 +12,37 @@ from livros.form import LivrosForm
 
  #CREATE 
 def createLivros(request):
-    formLivros = LivrosForm(request.POST or None)
-    if formLivros.is_valid() :
-        formLivros.save()
-        return redirect("")    
+    if request.user.is_authenticated:
+        formLivros = LivrosForm(request.POST or None)
+        if formLivros.is_valid() :
+            formLivros.save()
+            return redirect("/livrospretendidos")    
 
-    pacote = {"formLivros": formLivros}
-    return render(request, "Create/createLivros.html", pacote)
+        pacote = {"formLivros": formLivros}
+        return render(request, "Create/createLivros.html", pacote)
+    else:
+        return HttpResponse("É preciso estar logado")
 
 #UPDATE
 def updateLivros(request, id):
-    est = Livros.objects.get(pk=id)
-    formLivros = LivrosForm(request.POST or None, instance=est)
-    if formLivros.is_valid() :
-        formLivros.save()
-        return redirect("")    
+    if request.user.is_authenticated:
+        est = Livros.objects.get(pk=id)
+        formLivros = LivrosForm(request.POST or None, instance=est)
+        if formLivros.is_valid() :
+            formLivros.save()
+            return redirect("/livrospretendidos")    
 
-    pacote = {"formLivros": formLivros}
-    return render(request, "Create/createLivros.html", pacote)
+        pacote = {"formLivros": formLivros}
+        return render(request, "Create/createLivros.html", pacote)
+    else:
+        return HttpResponse("É preciso estar logado")
 #DELETE 
 
 def deleteLivros(request, id):
-    est = Livros.objects.get(pk=id)
-    est.delete()
-    return redirect("") 
+    if request.user.is_authenticated:
+        est = Livros.objects.get(pk=id)
+        est.delete()
+        return redirect("/livrospretendidos") 
 
 #LOGIN
 def login(request):
@@ -66,21 +73,28 @@ def cadastro(request):
             return HttpResponse("Ja existe um usuário com esse nome")
         user = User.objects.create_user(username=username, email=email, password=senha)
         user.save()    
-        return HttpResponse("Cadastrado")
+        return redirect("/")
 
 
 def listalivrospretendidos(request):
-    livros = Livros.objects.all().filter(liv_leitura = False) 
-    parametros = {
-        "livrospretendidos": livros
-    }
-    return render(request, "livrospretendidos.html", parametros)
-    
+    if request.user.is_authenticated:
+        livros = Livros.objects.all().filter(liv_leitura = False) 
+        parametros = {
+            "livrospretendidos": livros
+        }
+        return render(request, "livrospretendidos.html", parametros)
+    else:
+        return HttpResponse("É preciso estar logado")
+        
 def listalivroslidos(request):
-    livros = Livros.objects.filter(liv_leitura = True) 
-    parametros = {
-        "livroslidos": livros
-    }
-    return render(request, "table.html", parametros)
+    if request.user.is_authenticated:
+        livros = Livros.objects.filter(liv_leitura = True) 
+        
+        parametros = {
+            "livroslidos": livros
+        }
+        return render(request, "table.html", parametros)
+    else:
+        return HttpResponse("É preciso estar logado")
 # Create your views here.
 
